5318f8938182ca0ba045ec5b72594e1952bce892
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2021-12-26 12:03:57 -0500

    Finished Challenge

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        SpeedConverter.toMilesPerHour(10.5);
+        long miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        SpeedConverter.printConversion(10.5);
     }
 }
